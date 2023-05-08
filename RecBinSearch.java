2b9424d6a85322583c2b03ad7535ff1bc703bf6a
Adrijan Vranjkovic <adrijan.vranjkovic@stu.ocsb.ca>
2023-05-08 23:25:26 +0000

    RecBinSearch

--- RecBinSearch.java
+++ RecBinSearch.java
@@ -1,115 +1,130 @@
 // Importing
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
-
 /**
- * This program uses binary search.
- *
- * @author  Adrijan Vranjkovic
- * @version 1.0
- * @since   2023-05-03
- */
-public class RecBinSearch {
+* The program uses binary search to find a num.
+*
+* @author  Adrijan Vranjkovic
+* @version 1.0
+* @since   2023-05-03
+*/
 
+public final class RecBinSearch {
     /**
-     * For the style checker.
-     *
-     * @exception IllegalStateException Utility class.
-     * @see IllegalStateException
-     */
+    * For style checker.
+    *
+    * @exception IllegalStateException Utility class.
+    * @see IllegalStateException
+    */
     private RecBinSearch() {
         throw new IllegalStateException("Utility class");
     }
 
     /**
-     * This is the main function.
-     *
-     * @param args Unused
-     */
+    * Print messages.
+    *
+    * @param args Unused
+    */
     public static void main(String[] args) {
 
-        // Try statement.
+        // Create list
+        final ArrayList<String> inputList = new ArrayList<String>();
+        String indexStr = "";
+
+        // Try Statement.
         try {
 
-            // Create input and output file.
-            File inputFile = new File("input.txt");
-            File outputFile = new File("output.txt");
+            // Create the input file.
+            final File input = new File("input.txt");
+            final Scanner scanInput = new Scanner(input);
 
-            // Create scanner and writer
-            Scanner scanner = new Scanner(inputFile);
-            PrintWriter writer = new PrintWriter(outputFile);
+            // Create the output file.
+            final FileWriter output = new FileWriter("output.txt");
 
-            // Read each line of the input file.
-            for (int count = 0; count < 10; count++) {
-                
-                // Split into strings.
-                String[] numString = scanner.nextLine().split(" ");
-                String[] searchNumString = scanner.nextLine().split(" ");
+            // Loop through each line of the file
+            while (scanInput.hasNextLine()) {
+                inputList.add(scanInput.nextLine());
+            }
 
-                // Fill the list and sort it.
-                int[] listOfNum = new int[numString.length];
-                int searchNum = Integer.parseInt(searchNumString[0]);
+            // Create array
+            final String[] inputArr = new String[inputList.size()];
 
-                // Convert the list array
-                for (int index = 0; index < numString.length; index++) {
-                    listOfNum[index] = Integer.parseInt(numString[index]);
+            // Add the list elements to the array.
+            for (int location = 0; location < inputArr.length; location++) {
+                inputArr[location] = inputList.get(location);
+            }
+
+            // Send the input piece by piece to the function
+            for (int counter = 0; counter < inputArr.length; counter += 2) {
+
+                // Split the string.
+                final int[] arrOfInt =
+                    new int[inputArr[counter].split(" ").length];
+                for (int place = 0;
+                    place < inputArr[counter].split(" ").length; place++) {
+                    arrOfInt[place] = Integer.parseInt(
+                        inputArr[counter].split(" ")[place]);
                 }
 
                 // Sort the array
-                Arrays.sort(listOfNum);
+                Arrays.sort(arrOfInt);
 
-                int left = 0;
-                int right = listOfNum.length - 1;
+                // Parse to int
+                final int intSearch = Integer.parseInt(inputArr[counter + 1]);
 
-                // Call binary search function.
-                try {
-                    int result = RecBinSearch(listOfNum, searchNum, left, right);
-                    writer.println(result);
-                } catch (Exception e) {
-                    // Handle any errors here
-                    e.printStackTrace();
-                }
+                // Call function to find the search num in the array.
+                final int index = recBinSearch(arrOfInt, intSearch,
+                    0, arrOfInt.length - 1);
+                indexStr = indexStr + index + "\n";
             }
 
-            // Close scanner and writer.
-            scanner.close();
-            writer.close();
+            // Write to output file.
+            output.write(indexStr);
 
-        } catch (FileNotFoundException e) {
-            // Display error.
-            System.out.println("File not found!");
+            // Close output
+            output.close();
+
+        } catch (IOException err) {
+            // Display error
+            System.err.println("Error: " + err.getMessage());
         }
     }
 
-
     /**
-     * This function uses binary search recursively.
-     *
-     * @param listOfNum
-     * @param searchNum
-     * @param left
-     * @param right
-     * @return result
-     */
-    public static int RecBinSearch(int[] listOfNum, int searchNum, int left, int right) {
+    * This function uses binary search recursively.
+    *
+    * @param listOfNum *
+    * @param searchNum *
+    * @param left *
+    * @param right *
+    * @return result
+    */
+    public static int recBinSearch(int[] listOfNum, int searchNum,
+        int left, int right) {
+
+        // If left > right, the element is not in the array
+        if (left > right) {
+            return -1;
+        }
 
         // Set the mid value
-        int mid = left + (right - left) / 2;
+        final int mid = left + (right - left) / 2;
 
-        // If the search num is = to the mid num return mid.
+        // If the search num is equal to the mid num return mid.
         if (listOfNum[mid] == searchNum) {
             return mid;
 
-        // If searchNum is less than cut the array halfway.
+            // If searchNum is less than cut the array halfway.
         } else if (listOfNum[mid] > searchNum) {
-            return RecBinSearch(listOfNum, searchNum, mid-1, left);
-        
-        // If searchNum is more than cut the array halfway.
+            return recBinSearch(listOfNum, searchNum, left, mid - 1);
+
+            // If searchNum is more than cut the array halfway.
         } else {
-            return RecBinSearch(listOfNum, searchNum, mid+1, right);
+            return recBinSearch(listOfNum, searchNum, mid + 1, right);
         }
     }
 
